e5522607fba65dcf68cf3336ef399bd672e4591d
aromazyl <zyl2336709@gmail.com>
2017-07-01 22:08:25 +0800

    update

---
+++ libevent/helloworld.c
@@ -0,0 +1,20 @@
+/*
+ * helloworld.c
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <netinet/inet.h>
+#include <sys/socket.h>
+
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+#include <event2/event.h>
+
+static const char MESSAGE[] = "Hello World\n";
